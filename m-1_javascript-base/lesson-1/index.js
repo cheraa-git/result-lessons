901f33d4cd406ae5332a2f3a3bf7170d433a1417
cheraa-git <chernetsov.aa@mail.ru>
2022-11-08 20:09:06 +0400

    m-1_javascript-base lesson-1

---
+++ m-1_javascript-base/lesson-1/index.js
@@ -0,0 +1,5 @@
+console.log('Запускаем таймер')
+for (let i = 1; i <= 5; i++){
+  alert(i)
+  if (i === 5) console.log('Обратный отсчет закончен')
+}
---
+++ m-1_javascript-base/lesson-1/js-info.js
@@ -0,0 +1,5 @@
+alert('Брендан Эйх, а также Марк Андрессен и Билл Джой')
+alert('214 дней')
+alert('1995')
+alert('JavaScript - это не Java')
+alert('ECMAScript')
