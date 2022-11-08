fe88eab435aeca0f1c291676ad5d863f5f56df58
cheraa-git <chernetsov.aa@mail.ru>
2022-11-08 21:40:57 +0400

    m-1 lesson-2

---
+++ m-1_javascript-base/lesson-2/task_1-2/index.js
@@ -0,0 +1,17 @@
+// JavaScript-код с созданием переменных
+/*
+Автор
+Александр Чернецов
+ */
+
+const myName = 'Alexandr'
+const mySurname = 'Chernetsov'
+const myFavoriteDrink = 'water'
+const myFavoriteAnimal = 'dog'
+const myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
---
+++ m-1_javascript-base/lesson-2/task_3-4/car.js
@@ -0,0 +1,21 @@
+const carName = 'Mercedes'
+let maxCarSpeed = 250
+let carOwner = 'Alex'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = 240
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = 230
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = 225
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+carOwner = 'Andrew'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Petr'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Anna'
+console.log('У машины изменился владелец', carOwner)
