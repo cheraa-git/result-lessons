9a0c1cf20b8319c29b2ad658fe727bd310a80fa8
cheraa-git <chernetsov.aa@mail.ru>
2022-11-09 00:18:49 +0400

    m-1 lesson-3 types

---
+++ m-1_javascript-base/lesson-3_types/task-1/data-types.js
@@ -0,0 +1,26 @@
+const name = 'Alexandr'
+console.log(name, typeof name)
+
+const age = 20
+console.log(age, typeof age)
+
+const isMarried = false
+console.log(isMarried, typeof isMarried)
+
+const favoriteCar = {
+  model: 'Tesla',
+  acceleration: 3
+}
+console.log(favoriteCar, typeof favoriteCar)
+
+const currentCar = undefined
+console.log(currentCar, typeof currentCar)
+
+const currentJob = null
+console.log(currentJob, typeof currentJob)
+
+const id = Symbol('myId')
+console.log(id, typeof id)
+
+const currentLesson = 3n
+console.log(currentLesson, typeof currentLesson)
---
+++ m-1_javascript-base/lesson-3_types/task-2/index.js
@@ -0,0 +1,11 @@
+let temperature = 5
+temperature = '+5º(C)'
+alert(temperature)
+
+let timezone = "+4(GMT)"
+timezone = 4
+alert(timezone)
+
+let isRainy = false
+isRainy = null
+alert(isRainy)
