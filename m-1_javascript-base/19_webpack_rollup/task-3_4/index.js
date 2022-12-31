c2f325d6a3796e8e21ded22c6a271e7f237bf7e0
cheraa-git <chernetsov.aa@mail.ru>
2022-12-31 17:39:51 +0400

    m-1 19 rollup_webpack task 3-4

---
+++ m-1_javascript-base/19_webpack_rollup/task-3_4/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+
+import JS_IMAGE from './assets/javascript.png'
+
+const jsImgHtml = document.createElement('img')
+jsImgHtml.src = JS_IMAGE
+document.body.append(jsImgHtml)
+
